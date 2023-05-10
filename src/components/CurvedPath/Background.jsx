e057fc5ebb15104383c4b4009ffd1a7c96a35bbd
lsy20140 <lsy20140@naver.com>
2023-05-11 03:16:06 +0900

    AppPath 에셋, 디자인 추가 및 수정

--- src/components/CurvedPath/Background.jsx
+++ src/components/CurvedPath/Background.jsx
@@ -1,22 +1,44 @@
-import { Box, Environment, Sphere, useTexture } from '@react-three/drei'
+import { Environment, Sphere } from '@react-three/drei'
 import { Gradient, LayerMaterial } from 'lamina'
 import React from 'react'
 import * as THREE from 'three'
 
 export default function Background() {
+  const colorA = "#7474BF";
+  const colorB = "#D6A4A4";
+  const start = 0.2;
+  const end = -0.5;
+
   return (
     <>
-      <Environment files='/images/industrial_sunset_puresky_4k.hdr' background/>
-      <Sphere scale={[200, 200, 200]}>
-        {/* <LayerMaterial
-          lighting='physical'
-          transmission={1}
-          side={THREE.BackSide}
-          >
-          
-          <Gradient colorA={"blue"} colorB={"white"} axes={"y"} start={0} end={-5}/>
-        </LayerMaterial> */}
+      <Sphere scale={[400, 400, 400]}>
+        <LayerMaterial color={"#ffffff"} side={THREE.BackSide}>
+          <Gradient
+            colorA={colorA}
+            colorB={colorB}
+            axes={"y"}
+            start={start}
+            end={end}
+          />
+        </LayerMaterial>
       </Sphere>
+      <Environment resolution={256}>
+        <Sphere
+          scale={[100, 100, 100]}
+          rotation-y={Math.PI / 2}
+          rotation-x={Math.PI} // 구름 아래 부분이 어둡게 보여 자연스러움
+        >
+          <LayerMaterial color={"#ffffff"} side={THREE.BackSide}>
+            <Gradient
+              colorA={colorA}
+              colorB={colorB}
+              axes={"y"}
+              start={start}
+              end={end}
+            />
+          </LayerMaterial>
+        </Sphere>
+      </Environment>
     </>
   )
 }
--- src/components/CurvedPath/Bird.jsx
+++ src/components/CurvedPath/Bird.jsx
@@ -1,5 +1,6 @@
 import { useGLTF } from '@react-three/drei'
-import React from 'react'
+import { useFrame } from '@react-three/fiber'
+import React, { useRef } from 'react'
 
 export default function Bird(props) {
   const { nodes, materials } = useGLTF('/images/Flying_gull.glb')
@@ -10,7 +11,9 @@ export default function Bird(props) {
         geometry={nodes.Flying_seagull.geometry}
         material={materials.lambert5SG}
         scale={0.5}
-      />
+      >
+        <meshStandardMaterial color={"white"}/>
+      </mesh>
     </group>
   )
 }
