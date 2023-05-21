a49e09a2225c7504b360fa721b2fbbfab9f42a4e
lsy20140 <lsy20140@naver.com>
2023-05-21 14:34:19 +0900

    Add keyboardControls, meshes

--- src/AppRapierPhysics.jsx
+++ src/AppRapierPhysics.jsx
@@ -1,17 +1,40 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useMemo } from 'react'
 import Experience from './RapierPhysics/Experience'
 import { Canvas } from '@react-three/fiber'
 import { Physics } from '@react-three/rapier'
+import { KeyboardControls } from '@react-three/drei'
+
+export const Controls = {
+  forward: "forward",
+  back: "back",
+  left: "left",
+  right: "right",
+  jump: "jump"
+}
 
 export default function AppRapierPhysics() {
+
+  const map = useMemo(() =>
+    [
+      { name: Controls.forward, keys: ["ArrowUp", "KeyW"] },
+      { name: Controls.back, keys: ["ArrowDown", "KeyS"] },
+      { name: Controls.left, keys: ["ArrowLeft", "KeyA"] },
+      { name: Controls.right, keys: ["ArrowRight", "KeyD"] },
+      { name: Controls.jump, keys: ["Space"] },
+    ], []
+  )
+
   return (
-    <Canvas shadows camera={{position: [3, 3, 3], fov: 60}}>
-      <color attach="background" args={["#ececec"]} />
-      <Suspense>
-        <Physics debug>
-          <Experience/>
-        </Physics>
-      </Suspense>
-    </Canvas>
+    <KeyboardControls map={map}>
+      <Canvas shadows camera={{position: [3, 3, 3], fov: 60}}>
+        <color attach="background" args={["#ececec"]} />
+        <Suspense>
+          <Physics debug>
+            <Experience/>
+          </Physics>
+        </Suspense>
+      </Canvas>
+    </KeyboardControls>
+
   )
 }
--- src/RapierPhysics/Experience.jsx
+++ src/RapierPhysics/Experience.jsx
@@ -1,24 +1,107 @@
-import { Box, OrbitControls, Sphere } from '@react-three/drei'
-import { BallCollider, CuboidCollider, RigidBody } from '@react-three/rapier'
-import React from 'react'
+import { Box, OrbitControls, Sphere, useKeyboardControls } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber';
+import { BallCollider, CuboidCollider, RigidBody, quat } from '@react-three/rapier'
+import React, { useRef, useState } from 'react'
+import * as THREE from 'three'
+import { Controls } from '../AppRapierPhysics';
 
 export default function Experience() {
+  const [hover, setHover] = useState(false)
+  const cube = useRef();
+  const [start, setStart] = useState(false)
+  const kicker = useRef();
+
+  const jump = () =>{
+    if(isOnFloor.current){
+      cube.current.applyImpulse({x: 0, y: 5, z: 0})
+    }
+  }
+
+  const jumpPressed = useKeyboardControls((state) => state[Controls.jump])
+  const leftPressed = useKeyboardControls((state) => state[Controls.left])
+  const rightPressed = useKeyboardControls((state) => state[Controls.right])
+  const backPressed = useKeyboardControls((state) => state[Controls.back])
+  const forwardPressed = useKeyboardControls((state) => state[Controls.forward])
+
+  const handleMovement = () => {
+    if(!isOnFloor.current){
+      return;
+    }
+
+    if(rightPressed) {
+      cube.current.applyImpulse({x: 0.1, y: 0, z: 0})
+    }
+    if(leftPressed) {
+      cube.current.applyImpulse({x: -0.1, y: 0, z: 0})
+    }
+    if(backPressed) {
+      cube.current.applyImpulse({x: 0, y: 0, z: 0.1})
+    }
+    if(forwardPressed) {
+      cube.current.applyImpulse({x: 0, y: 0, z: -0.1})
+    }
+  }
+
+  const speed = useRef(5)
+
+
+  useFrame((_state, delta) => {
+    if(jumpPressed) {
+      jump();
+    }
+    handleMovement();
+
+    if(!start) {
+      return;
+    }
+    const curRotation = quat(kicker.current.rotation());
+    const incrementRotation = new THREE.Quaternion().setFromAxisAngle(
+      new THREE.Vector3(0, 1, 0),
+      delta * 4
+    )
+    curRotation.multiply(incrementRotation)
+    kicker.current.setNextKinematicRotation(curRotation);
+    
+    speed.current += delta
+  })
+
+  const isOnFloor = useRef(true);
+
+
   return (
     <>
       <ambientLight intensity={0.5}/>
       <directionalLight position={[-10, 10, 0]} intensity={0.4}/>
       <OrbitControls />
-      <RigidBody position={[0, 5, 0]} colliders={false} gravityScale={4}>
-        <BallCollider args={[1]} position={[0, 1, 0]}/>
-        <Sphere position-y={1}>
-          <meshStandardMaterial color={"hotpink"} />
-        </Sphere>
-        <CuboidCollider args={[0.5, 0.5, 0.5]} />
-        <Box>
-          <meshStandardMaterial color="royalblue"/>
+
+      <RigidBody position={[-2.5, 1, 0]} ref={cube} 
+        onCollisionEnter={({other}) => {
+          if(other.rigidBodyObject.name === "floor") {
+            isOnFloor.current = true;
+          }
+        }}
+        onCollisionExit={({other}) => {
+          if(other.rigidBodyObject.name === "floor") {
+            isOnFloor.current = false;
+          }
+        }}
+      >
+        <Box 
+          onPointerEnter={() => setHover(true)} 
+          onPointerLeave={() => setHover(false)}
+          onClick={()=> setStart(true)}
+        >
+          <meshStandardMaterial color={hover ? "hotpink" : "royalblue"} />
         </Box>
       </RigidBody>
-      <RigidBody type='fixed' restitution={2}>
+      <RigidBody type='kinematicPosition' position={[0, 0.75, 0]} ref={kicker}>
+        <group position={[2.5, 0, 0]}>
+          <Box args={[5, 0.5, 0.5]}>
+            <meshStandardMaterial color='peachpuff' />
+          </Box>
+        </group>
+      </RigidBody>
+      <RigidBody type='fixed' name='floor'>
         <Box position={[0, 0, 0]} args={[10, 1, 10]} >
           <meshStandardMaterial color="springgreen" />
         </Box>
